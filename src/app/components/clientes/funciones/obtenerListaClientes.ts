d14d0197c73d9d7101b01aa42d110bea1750daff
RoxanaFernandez <roxana.jacquelinefa@fpuna.edu.py>
2022-11-24 12:30:55 -0300

    Se modificó el nombre de obtenerlistaclientes.ts

--- src/app/components/clientes/funciones/obtenerListaClientes.ts
+++ src/app/components/clientes/funciones/obtenerListaClientes.ts
@@ -1,6 +1,7 @@
+import { Cliente } from "src/app/models/cliente";
 import { listaClientes } from "../listaClientes";
 
-export function obtenerListaClientes(){
+export function obtenerListaClientes() : Cliente[] {
     if (localStorage.getItem('listaClientes')==null) 
         localStorage.setItem('listaClientes', JSON.stringify(listaClientes));
     return JSON.parse(localStorage.getItem('listaClientes')!);
